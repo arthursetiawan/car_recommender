09d4dcac86bbce7bf3c2761162d5727141264793
Arthur Setiawan <setiawan.arthur@yahoo.com>
2025-03-24 21:59:54 -0700

    skiski

--- backend/scraper/scrape_cargurus.py
+++ backend/scraper/scrape_cargurus.py
@@ -88,7 +88,7 @@ def scrape_cargurus(base_url, max_pages=5):
         logger.info(f"Scraping page {page}...")
         
         try:
-            # Get new headers for each request bro
+            # Get new headers for each request boskiski
             headers = get_random_headers()
             
             # Add a refer that looks like we're coming from previous pages
